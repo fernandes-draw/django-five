c73c2fc28027fdde1a62a86b6253d3421c0e00e5
Everson Fernandes <fernandes_draw@hotmail.com>
2026-02-11 23:05:55 -0300

    email, comments

--- blog/mysite/blog/models.py
+++ blog/mysite/blog/models.py
@@ -23,8 +23,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=2, choices=Status, default=Status.DRAFT)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     objects = models.Manager()
     published = PublishedManager()
 
@@ -40,10 +39,24 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse(
             "blog:post_detail",
-            args=[
-                self.publish.year,
-                self.publish.month,
-                self.publish.day,
-                self.slug
-            ]
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["created"]
+        indexes = [
+            models.Index(fields=["created"]),
+        ]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -18,20 +21,18 @@ def post_list(request):
     except PageNotAnInteger:
         # If page_number is not an integer get the first page
         posts = paginator.page(1)
-    return render(
-        request,
-        "blog/post/list.html",
-        {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 class PostListView(ListView):
     """
     Alternative post list view
     """
+
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
@@ -41,10 +42,75 @@ def post_detail(request, year, month, day, post):
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day)
+        publish__day=day,
+    )
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    # Form for users to comment
+    form = CommentForm()
 
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post}
+        {"post": post, "comments": comments, "form": form},
+    )
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    sent = False
+
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']}) " f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
     )
