47b17f3a7fa064bd7053029ca8ccf193d1e9d998
Everson Fernandes <fernandes_draw@hotmail.com>
2026-02-24 22:17:39 -0300

    Implementing blog module

--- mysyte/blog/models.py
+++ mysyte/blog/models.py
@@ -3,26 +3,35 @@ from django.utils import timezone
 from django.conf import settings
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-        max_length=2, choices=Status.choices, default=Status.DRAFT)
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
-        ordering = ('-publish',)
+        ordering = ("-publish",)
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=["-publish"]),
         ]
 
     def __str__(self):
