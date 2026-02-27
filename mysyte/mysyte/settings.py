43819b053de808c433d3797bcacc1198fdbe3a88
fernandes-draw <fernandes_draw@hotmail.com>
2026-02-27 15:50:22 -0300

    Adding a sitemap to the site

--- mysyte/mysyte/settings.py
+++ mysyte/mysyte/settings.py
@@ -28,12 +28,16 @@ ALLOWED_HOSTS = []
 # third-party packages in the middle,
 # and local applications at the end of INSTALLED_APPS.
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
     "django.contrib.staticfiles",
 
     # Third part
