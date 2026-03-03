24eba5a1d3f46fd38f2f93bccd5c252425efa1b7
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-03 20:40:53 -0300

    Creating a detail view for images

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'images'
+app_name = "images"
 
 
 urlpatterns = [
-    path('create/', views.image_create, name='create'),
+    path("create/", views.image_create, name="create"),
+    path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
 ]
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,11 +2,13 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
 
 
 @login_required
 def image_create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # form is sent
         form = ImageCreateForm(data=request.POST)
 
@@ -15,12 +17,12 @@ def image_create(request):
             cd = form.cleaned_data
             new_image = form.save(commit=False)
 
-          # assign current user to the item
+            # assign current user to the item
             new_image.user = request.user
             new_image.save()
-            messages.success(request, 'Image added successfully')
+            messages.success(request, "Image added successfully")
 
-          # redirect to new created item detail view
+        # redirect to new created item detail view
         return redirect(new_image.get_absolute_url())
 
     else:
@@ -28,7 +30,12 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
 
     return render(
-        request,
-        'images/image/create.html',
-        {'section': 'images', 'form': form}
+        request, "images/image/create.html", {"section": "images", "form": form}
+    )
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request, "images/image/detail.html", {"section": "images", "image": image}
     )
