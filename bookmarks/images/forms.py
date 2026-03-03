0b3272155dcbb311c4cc5e87502e31ac56fb0ead
fernandes-draw <fernandes_draw@hotmail.com>
2026-03-03 13:05:07 -0300

    Cleaning form fields Installing the Requests library

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -9,3 +9,17 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extension = url.rsplit('.', 1)[1].lower()
+
+        if extension not in valid_extensions:
+            raise forms.ValidationError(
+                'The given URL does not match valid image extensions.')
+
+        return url
+
+# Installing the Requests library
+# python -m pip install requests==2.31.0
